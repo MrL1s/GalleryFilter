7156612f7f4a71608c303df76455593acb879bee
lis <egorkorkin9@gmail.com>
2025-08-21 22:34:32 +0400

    TwoVersion

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import { router } from "./router";
 import "@/assets/styles/tailwind.css"; // Tailwind
-import "@/assets/styles/main.scss"; // глобальные SASS стили
-
-// оставь свой импорт стилей как у тебя настроено:
-// например: import "@/assets/main.css";  или  import "@/assets/main.scss";
+import "@/assets/styles/main.scss"; // global SASS style
 
 createApp(App).use(router).mount("#app");
